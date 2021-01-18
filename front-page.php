f1d59f66534e743a7f68f069fd9597383ab8aacb
RemiBLoizeau <74113050+RemiBLoizeau@users.noreply.github.com>
2021-01-17 21:24:47 -0400

    Patch RGPD

--- front-page.php
+++ front-page.php
@@ -6,8 +6,9 @@ Template Name:  Page d'accueil
 get_header(); ?>
 
 <section class="row">
-  <div class="col l12">
+  <div class="col l6 xl6">
     <h1><?php the_title(); ?></h1>
+    <p class="headline">Peut-être est-ce difficile à croire, mais ici se tiendra bientôt fièrement le site <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>. Pour le moment, vous avez simplement sous les yeux Monobloc.</p>
   </div>
   <?php get_template_part( 'parts/elements' ); ?>
 </section>
--- parts/rgpd.php
+++ parts/rgpd.php
@@ -6,10 +6,10 @@
 ?>
 
 <div id="message_rgpd" class="rgpd is--open">
-  <div class="rgpd__message">
+  <div class="rgpd__message col l8 xl8">
     <p class="body">En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies ou autres traceurs pour réaliser des statistiques de visites et obtenir des informations sur votre parcours de navigation afin d’améliorer nos contenus et votre expérience utilisateur. Plus d’informations en <a href="<?php echo home_url(); ?>/informations-legales">cliquant ici</a>.</p>
   </div>
-  <div class="rgpd__btn">
+  <div class="rgpd__btn col l4 xl4">
     <span class="btn btn--plain rgpd__oui">OK</span>
     <a href="<?php echo home_url(); ?>/informations-legales" class="btn btn--outline">En savoir plus</a>
     <a href="<?php echo home_url(); ?>" class="btn btn--outline">Décliner</a>
