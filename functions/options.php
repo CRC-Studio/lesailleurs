b71235054c0238cb99004c09a259bc87cb7354c5
YokoHomareda <yokohomareda@gmail.com>
2021-03-08 18:14:58 -0400

    RGPD cookies fonction

--- functions/options.php
+++ functions/options.php
@@ -13,3 +13,21 @@
 */
 
 add_theme_support( 'post-thumbnails' );
+
+
+/**
+* Support de Cookies necessaires en version francais ( vertsion anglais fait autumatiquement.)
+*  https://www.webtoffee.com/modify-strictly-enabled-cookie-categories-using-webtoffee-gdpr-cookie-consent-plugin/
+*/
+
+
+function webtoffee_strictly_enabled_cookie_categories($strict_categories) {
+
+    $count = count($strict_categories);
+    $strict_categories[$count] = 'necessaire';
+    $strict_categories[$count+1] = 'necessary';
+
+    return $strict_categories;
+}
+
+add_filter('gdpr_strictly_enabled_category', 'webtoffee_strictly_enabled_cookie_categories');
